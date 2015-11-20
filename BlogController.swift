ce84193a14b484edc838462f1d1e7f39c059f13a
Thang Tong <tongthang0104@gmail.com>
2015-11-20 14:08:26 -0700

    BlogController updated
    implement completion closure with currentBlog

--- BlogController.swift
+++ BlogController.swift
@@ -12,29 +12,34 @@ class BlogController {
     
     static var currentBlog: [Blog] = []
     // fetch all Blogs
-    static func fetchPosts(user: User, completion: (post: [Blog]) -> Void) {
-        
+    static func fetchBlogs(user: User, completion: (blog: [Blog]) -> Void) {
+        completion(blog: currentBlog)
     }
 
     // create Blog
     static func createBlog(user: User, videoEndPoint: String, caption: String?, completion: (blog: Blog?, success: Bool) -> Void){
-        
+        completion(blog: currentBlog.first, success: true)
     }
     // Blog From Identifier
     static func blogFromIdentifier(identifier: String, completion: (blog: Blog?) -> Void) {
-        
+        completion(blog: currentBlog.first)
     }
     
     
     // remove Blog
-    
+    static func removeBlog(blog: Blog?, completion: (success: Bool) -> Void) {
+        completion(success: true)
+    }
 
     // add comment Blogs
     static func addCommentToBlog(user: User, comment: [Comment]?, completion: (blog: Blog?, success: Bool) -> Void) {
-        
+        completion(blog: currentBlog.first, success: true)
     }
     
     // like Blogs
+    static func likeBlogs(user: User, like: [Like]?, completion: (blog: Blog?, success: Bool) -> Void) {
+        completion(blog: currentBlog.first, success: true)
+    }
     
     
     

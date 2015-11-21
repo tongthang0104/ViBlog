439dfdbabc925b5245cc3360bcca322dd9855511
Thang Tong <tongthang0104@gmail.com>
2015-11-20 23:22:43 -0700

    segue from signuploginpickerView to matching View

--- SignUpLoginPickerViewController.swift
+++ SignUpLoginPickerViewController.swift
@@ -9,37 +9,31 @@
 import UIKit
 
 class SignUpLoginPickerViewController: UIViewController {
-
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func signUpButtonTapped(sender: UIButton) {
-        performSegueWithIdentifier("toSignUpView", sender: self)
-    }
     
-    @IBAction func loginButtonTapped(sender: UIButton) {
-        performSegueWithIdentifier("toLoginView", sender: self)
-    }
-  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let destinationController = segue.destinationViewController as? SignUpLoginViewController {
+            if segue.identifier == "toSignUpView" {
+                destinationController.mode = .Signup
+            } else if segue.identifier == "toLoginView" {
+                destinationController.mode = .Login
+            }
+        }
     }
-    */
-
 }

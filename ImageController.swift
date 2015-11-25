96ee0e50171a10ef9a0077b65a9921f7712bd753
Thang Tong <tongthang0104@gmail.com>
2015-11-24 20:50:27 -0700

    implement imageController

---
+++ ImageController.swift
@@ -0,0 +1,23 @@
+//
+//  ImageController.swift
+//  ViBlog
+//
+//  Created by Thang H Tong on 11/23/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ImageController {
+    
+    static func uploadImage (image: UIImage, completion: (identifier: String?) -> Void) {
+        
+    }
+    
+    static func imageForIdentifier(identifer: String, completion: (image: UIImage?) -> Void) {
+        
+    }
+    
+}

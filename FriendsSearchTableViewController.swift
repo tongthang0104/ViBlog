2e78c0a0ffbdae692cb9027eb7fc8e6984a495b7
Thang Tong <tongthang0104@gmail.com>
2015-11-24 20:51:13 -0700

    Search Controller implemented

--- FriendsSearchTableViewController.swift
+++ FriendsSearchTableViewController.swift
@@ -8,95 +8,152 @@
 
 import UIKit
 
-class FriendsSearchTableViewController: UITableViewController {
-
-
-    var user: User?
+class FriendsSearchTableViewController: UITableViewController, UISearchResultsUpdating {
     
+    // MARK: Properties
+    
+    var searchController: UISearchController!
+    var user: User?
+//    
+//    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    enum ViewMode: Int {
+        case Friends = 0
+        case AllChannels = 1
+        
+        func users(completion: (users: [User]?) -> Void) {
+            
+            switch self {
+            case .Friends:
+                UserController.followedByUser(UserController.shareController.currentUser, completion: { (followers) -> Void in
+                    completion(users: followers)
+                })
+            case .AllChannels:
+                UserController.fetchAllUsers({ (users) -> Void in
+                    completion(users: users)
+                })
+            }
+        }
+    }
+    
+    // MARK: Search Controller
+    
+    func setUpSearchController() {
+        let resultsViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("userSearchResult")
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController.searchBar.sizeToFit()
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        definesPresentationContext = true
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        guard let searchTerm = searchController.searchBar.text?.lowercaseString else {return}
+        let resultsViewController = searchController.searchResultsController as! UserSearchResultTableViewController
+        resultsViewController.filterUsers = UserController.mockUsers().filter({$0.username.lowercaseString.containsString(searchTerm)})
+        resultsViewController.tableView.reloadData()
+    }
     
     
+    var mode: ViewMode {
+        get {
+            return ViewMode(rawValue: segmentControl.selectedSegmentIndex)!
+        }
+    }
     
+    var userDataSource: [User] = []
     
+    // MARK: Update With Function
     
-    
+    func updateBaseOnMode() {
+        mode.users { (users) -> Void in
+            if let users = users {
+                self.userDataSource = users
+            } else {
+                self.userDataSource = []
+            }
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.tableView.reloadData()
+            })
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        updateBaseOnMode()
+        setUpSearchController()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return userDataSource.count
     }
-
-    /*
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("friendsCell", forIndexPath: indexPath) as! FriendsTableViewCell
+        
+        let users = userDataSource[indexPath.row]
+    
+        cell.updateWithUsers(users)
+        
         return cell
     }
-    */
-
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // Return false if you do not want the specified item to be editable.
+    return true
     }
     */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    // Return false if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- UserSearchResultTableViewController.swift
+++ UserSearchResultTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class UserSearchResultTableViewController: UITableViewController {
 
+    
+    var filterUsers = [User] ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,25 +30,21 @@ class UserSearchResultTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return filterUsers.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("userSearchCell", forIndexPath: indexPath) as! FriendsTableViewCell
+        let users = filterUsers[indexPath.row]
+        cell.updateWithUsers(users)
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.

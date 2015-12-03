828ea20c5884e110aaf8d2ca84684dc826267410
Thang Tong <tongthang0104@gmail.com>
2015-12-03 12:28:54 -0700

    appicon asset

---
+++ VideoBlogTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoBlogTableViewCell.swift
+//  ViBlog
+//
+//  Created by Thang H Tong on 11/28/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import UIKit
+
+class VideoBlogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

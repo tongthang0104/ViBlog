974619c11b68a2af03651d15e94c1a350530ce17
Thang Tong <tongthang0104@gmail.com>
2015-12-03 12:29:11 -0700

    videoController update

--- VideoController.swift
+++ VideoController.swift
@@ -7,12 +7,21 @@
 //
 
 import Foundation
+import AVFoundation
+import UIKit
 
 class VideoController {
     
 
     // upload Videos
-    
+    static func uploadVideo(video: AVPlayer, completion: (identifier: String?)-> Void) {
+        
+        
+    }
+
     // Video for ID
+    static func videoForID(identifier: String, completion: (video: AVPlayer?) -> Void) {
+        
+    }
     
 }

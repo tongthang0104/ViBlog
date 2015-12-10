fa371757702c5ec6c520988ce8bc556e235d12c1
Thang Tong <tongthang0104@gmail.com>
2015-12-10 00:22:22 -0700

    Updated BlogController

--- BlogChannelTableViewController.swift
+++ BlogChannelTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Parse
 
 class BlogChannelTableViewController: UITableViewController {
-
+    
     var  blogs: [Blog] = []
     let users: [User] = []
     
@@ -21,36 +21,31 @@ class BlogChannelTableViewController: UITableViewController {
     }
     
     @IBAction func userRefreshTableView(sender: UIRefreshControl) {
-        loadBlogChannels(UserController.shareController.currentUser)
+        loadBlogChannels(UserController.shareController.current!)
     }
-  
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
         
         // Check if there is User, if there is no user, go to SignUpLoginPickerView
         if let currentUser = PFUser.currentUser() {
-            if currentUser.authenticated {
+            if blogs.count > 0 {
                 loadBlogChannels(currentUser)
             }
-//            if blogs.count > 0 {
-//                loadBlogChannels(currentUser)
-//            }
-            
         } else {
             tabBarController?.performSegueWithIdentifier("noCurrentUserSegue", sender: nil)
         }
     }
     
     func loadBlogChannels(user: PFUser) {
-//        BlogController.fetchBlogsForUser(user) { (blog) -> Void in
-//            if let blog = blog {
-//                self.blogs = blog
-//                
-//                self.tableView.reloadData()
-//                self.refreshControl?.endRefreshing()
-//
-//            }
-//        }
+        BlogController.fetchBlogsForUser(user) { (blog) -> Void in
+            if let blog = blog {
+                self.blogs = blog
+                
+                self.tableView.reloadData()
+                self.refreshControl?.endRefreshing()
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -59,65 +54,65 @@ class BlogChannelTableViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return blogs.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("videoBlogCell", forIndexPath: indexPath) as! VideoBlogTableViewCell
         
         let blog = blogs[indexPath.row]
         cell.updateWithBlogs(blog)
-
+        
         return cell
     }
     
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // Return false if you do not want the specified item to be editable.
+    return true
     }
     */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    // Return false if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }

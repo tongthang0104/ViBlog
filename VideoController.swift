305a0e7dc1e6911c75f3320a7a4531f37f2d4047
Thang Tong <tongthang0104@gmail.com>
2015-12-09 17:23:26 -0700

    code cleaning

--- VideoController.swift
+++ VideoController.swift
@@ -11,15 +11,29 @@ import AVFoundation
 import UIKit
 
 class VideoController {
-    
-
-    
+        
     // upload Videos
     static func uploadVideo(video: PFFile, completion: (identifier: String?)-> Void) {
         
         completion(identifier: "-k12312492hfnasd")
     }
 
+    
+    static func takeSnapshot(url: NSURL) {
+        let asset: AVAsset = AVAsset(URL: url)
+        let duration: CMTime = asset.duration
+        let snapshot = CMTimeMake(duration.value / 2, duration.timescale)
+        
+        let generator = AVAssetImageGenerator(asset: asset)
+        let imageRef: CGImageRef = try! generator.copyCGImageAtTime(snapshot, actualTime: nil)
+        
+        let thumbnail: UIImage = UIImage(CGImage: imageRef)
+        let data = UIImageJPEGRepresentation(thumbnail, 0.8)
+        //let data = UIImagePNGRepresentation(thumbnail)
+        let directory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        data?.writeToFile(directory, atomically: true)
+    }
+    
     // Video for ID
     static func videoForID(identifier: String, completion: (video: AVPlayer?) -> Void) {
         

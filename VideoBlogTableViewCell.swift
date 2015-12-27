25f50b35d8cb08574f028b31e7e42e2357df68a4
Thang Tong <tongthang0104@gmail.com>
2015-12-27 12:43:46 -0500

    add Bond to Pod

--- VideoBlogTableViewCell.swift
+++ VideoBlogTableViewCell.swift
@@ -12,6 +12,9 @@ import AVKit
 import MobileCoreServices
 import AVFoundation
 import Parse
+import Bond
+
+
 
 
 class VideoBlogTableViewCell: UITableViewCell, BlogChannelTableViewControllerDelegate {
@@ -20,9 +23,30 @@ class VideoBlogTableViewCell: UITableViewCell, BlogChannelTableViewControllerDel
     
     var user: User?
     var like: Like?
-        var likeArray: [Like] = []
+    var likeArray: [Like] = []
     var caption: String?
-    var blog: Blog!
+    var blog: Blog! 
+//        didSet {
+//            // free memory of image stored with post that is no longer displayed
+//            if let oldValue = oldValue where oldValue != blog {
+////                likeBond.unbindAll()
+//                videoView.designatedBond.unbindAll()
+//                postImageView.designatedBond.unbindAll()
+//                if (oldValue.image.bonds.count == 0) {
+//                    oldValue.image.value = nil
+//                }
+//            }
+//            
+//            if let post = post {
+//                // bind the image of the post to the 'postImage' view
+//                post.image ->> postImageView
+//                
+//                // bind the likeBond that we defined earlier, to update like label and button when likes change
+//                post.likes ->> likeBond
+//            }
+//        }
+//
+//    }
     var videoOfUrl: NSURL?
     
     @IBOutlet weak var likeButton: UIButton!
@@ -111,16 +135,16 @@ class VideoBlogTableViewCell: UITableViewCell, BlogChannelTableViewControllerDel
         }
         
         self.likeLabel.text = "\(likeArray.count) likes"
-
-//        if let  like = BlogChannelTableViewController.shareController.like {
-//        self.likeLabel.text = "\(like.count) likes"
-//        } else {
-//            BlogChannelTableViewController.shareController.like = []
-//        }
-
-            BlogController.likeForBlog(blog) { (like) -> Void in
-               self.likeArray = like
-            }
+        
+        //        if let  like = BlogChannelTableViewController.shareController.like {
+        //        self.likeLabel.text = "\(like.count) likes"
+        //        } else {
+        //            BlogChannelTableViewController.shareController.like = []
+        //        }
+        
+        BlogController.likeForBlog(blog) { (like) -> Void in
+            self.likeArray = like
+        }
     }
     
     //MARK: - AV Player
